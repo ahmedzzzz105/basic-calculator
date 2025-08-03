faeb461a6798051ae851be22cb445e9454e6ae4c
ahmedzzzz105 <ahmed.k261007@gmail.com>
2025-08-03 11:43:09 +0530

    Add basic calculator code in C++
    Uploaded a simple calculator program that performs addition, subtraction , multiplication and division with error handling for division by zero.

---
+++ code.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a , b ;
+    cout << " enter the first number : "  << endl;
+    cin  >> a ;
+    cout << " enter the second  number : "  << endl;
+    cin >> b;
+    
+    
+    
+   
+    
+    
+    int choice ;
+    
+    int sum = a+b;
+    int difference = a - b;
+    int product = a * b;
+    
+
+    cout << "welcome to online calculator" << endl;
+    cout << "pls select the operation you want to perform"  <<   endl;
+    cout << "1. addition\n" << "2. subtraction\n" <<  "3. multiplication\n" << "4. division\n" << endl;
+    cin >> choice ;
+    if (choice == 1){
+    cout << " the sum is : " << sum << endl;
+     } else if (choice == 2){
+    cout << " the difference is : " << difference << endl;
+     } else if (choice == 3){
+    cout << " the product is : " << product << endl;
+     } else if (choice == 4 ){ 
+          if ( b == 0) {
+             cout << " cannot divide by zero" << endl;
+         } else {
+              int quotient =  a / b;
+    
+              cout << " the quotient is : " << quotient << endl;
+         }   
+      } else {
+           cout << "invalid choice" << endl;
+      }
+        return 0;
+}
+    
+     
